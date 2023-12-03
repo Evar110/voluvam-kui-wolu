4152b6291d77fb4c19bc34d47a1e02eb4a38c369
rnn00 <github.ed91r@simplelogin.co>
2023-12-03 10:12:26 +0200

    Kommenteerisin JS

--- functions.js
+++ functions.js
@@ -1,13 +1,13 @@
 // Navigatsiooniriba: https://www.w3schools.com/howto/howto_js_topnav_responsive.asp
 // Eraldi JS fail: https://blog.hubspot.com/website/call-javascript-function-html
-var button = document.getElementById("icon");
-button.addEventListener("click", myFunction);
+var button = document.getElementById("icon"); // otsib icon-klassi objekti
+button.addEventListener("click", myFunction); // "kuulaja", käivitab funktsiooni alles vajutamisel
 
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
+function myFunction() { // defineerime funktsiooni
+    var x = document.getElementById("myTopnav"); // leia element ID-ga "topnav"
+    if (x.className === "topnav") { // kui objekt, millel funktsiooni välja kutsusime, on "topnav"
+        x.className += " responsive"; // siis lisab CSS tingimuse "responsive"
     } else {
-        x.className = "topnav";
+        x.className = "topnav"; // vastasel juhul määrab objekti topnav-iks
     }
 }
